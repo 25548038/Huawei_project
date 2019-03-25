977f2e0e9ad9b74e5ad9b09c44bb19eebf00b4b8
25548038 <25548038@qq.com>
2019-03-25 09:50:09 +0800

    修改冲突

--- app/js/shop_cat.js
+++ app/js/shop_cat.js
@@ -4,22 +4,23 @@ define(['jquery', 'common/common'], function($, public) {
         init(){
             $('.top-header').load('public.html .shortcut');
             $('.foot-box').load('public.html ._maxfoot');
+            $('.ico-fied').load('public.html .hungBar');
             this.event();
 
         },
         event(){
-            $('.p-service-info').on('mouseenter',function(){
-                $('.p-list').css("display","block");
-                $('.p-service-info').css("background","#FFF");
-                $('.p-service-info').children('a').addClass('p-service-hov')
+            // $('.p-service-info').on('mouseenter',function(){
+            //     $('.p-list').css("display","block");
+            //     $('.p-service-info').css("background","#FFF");
+            //     $('.p-service-info').children('a').addClass('p-service-hov')
 
-            })
-            $('.p-service-info').on('mouseleave',function(){
-                $('.p-list').css("display","none")
-                $('.p-service-info').css("background","none")
-                $('.p-service-info').children('a').removeClass('p-service-hov')
+            // })
+            // $('.p-service-info').on('mouseleave',function(){
+            //     $('.p-list').css("display","none")
+            //     $('.p-service-info').css("background","none")
+            //     $('.p-service-info').children('a').removeClass('p-service-hov')
 
-            })
+            // })
             $('.left').on('click',function(){
                 $('.swiper-list').css('left',-200)
             })
@@ -30,9 +31,7 @@ define(['jquery', 'common/common'], function($, public) {
                 let flag = true,
                 input = $('.sc-pro-list').find('.input');
                 input.each(function(){
-                    console.log(1)
                   if(input.hasClass('checked')){
-                    console.log(1)
                     flag = false;
                     return false;
                   }
