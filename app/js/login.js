7b5ece0ff6e80e69a6933569ed3d3d10ffe9efed
liubynb <826711990@qq.com>
2019-03-25 14:18:54 +0800

    登入功能静态页面

---
+++ app/js/login.js
@@ -0,0 +1,8 @@
+$('.login-left').on("click",function(){
+    $('.login-hiden').hide();
+    $('.login-form').show();
+})
+$('.login-right').on("click",function(){
+    $('.login-form').hide();
+    $('.login-hiden').show();
+})
